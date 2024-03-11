b7cf65d2862d6dae77bc667bfb0ee947b35271cf
Grauly <nils.dellemann@web.de>
2024-03-11 16:55:47 +0100

    feat: added base project

---
+++ src/client/java/grauly/chatignorer/ChatIgnorerClient.java
@@ -0,0 +1,10 @@
+package grauly.chatignorer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChatIgnorerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
