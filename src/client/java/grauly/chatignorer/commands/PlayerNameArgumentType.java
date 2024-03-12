8279756a4930ec8a253ba09893e3df556278d463
Grauly <nils.dellemann@web.de>
2024-03-12 03:30:07 +0100

    feat: show actual nick names

--- src/client/java/grauly/chatignorer/commands/PlayerNameArgumentType.java
+++ src/client/java/grauly/chatignorer/commands/PlayerNameArgumentType.java
@@ -11,6 +11,7 @@ import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.command.CommandSource;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class PlayerNameArgumentType implements ArgumentType<String> {
@@ -21,8 +22,12 @@ public class PlayerNameArgumentType implements ArgumentType<String> {
 
     public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
         if (!(context.getSource() instanceof FabricClientCommandSource source)) return Suggestions.empty();
-        CommandSource.suggestMatching(source.getPlayerNames(), builder);
+        CommandSource.suggestMatching(getPlayerNames(source), builder);
         CommandSource.suggestMatching(AutoConfig.getConfigHolder(ChatIgnorerConfig.class).getConfig().ignoredPlayers, builder);
         return builder.buildFuture();
+    }
+
+    private List<String> getPlayerNames(FabricClientCommandSource source) {
+        return source.getClient().getNetworkHandler().getListedPlayerListEntries().stream().map(p -> p.getDisplayName().getString()).map(p -> p.replaceAll("\\[.*\\] ", "")).toList();
     }
 }
